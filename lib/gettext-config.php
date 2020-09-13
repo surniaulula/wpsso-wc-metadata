09584223dc308a8dfef654e6d049689e3b8711e4
JS Morisset <jsm@surniaulula.com>
2020-09-13 10:57:47 -0700

    v1.5.0

--- lib/gettext-config.php
+++
@@ -1,7 +0,0 @@
-<?php die( 'These aren\'t the droids you\'re looking for.' );
-
-/**
- * Extract of translatable text strings from the static config array.
- */
-_x( 'WooCommerce Metadata', 'lib file description', 'wpsso-wc-metadata' );
-_x( 'GTIN, GTIN-8, GTIN-12 (UPC), GTIN-13 (EAN), GTIN-14, ISBN, MPN, Depth, and Volume for WooCommerce Products and Variations.', 'plugin description', 'wpsso-wc-metadata' );
