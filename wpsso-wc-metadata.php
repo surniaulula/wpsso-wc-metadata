ed0861308b3f5b035afa915cd621f2ee9dba8841
JS Morisset <jsm@surniaulula.com>
2020-10-22 10:15:29 -0700

    v1.6.1

--- wpsso-wc-metadata.php
+++ wpsso-wc-metadata.php
@@ -14,7 +14,7 @@
  * Requires PHP: 5.6
  * Requires At Least: 4.4
  * Tested Up To: 5.5.1
- * WC Tested Up To: 4.6.0
+ * WC Tested Up To: 4.6.1
  * Version: 1.6.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
