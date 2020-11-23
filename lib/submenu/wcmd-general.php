d3d63d194a0d7e6b39fc02f24829e252d00c4480
JS Morisset <jsm@surniaulula.com>
2020-11-23 13:09:21 -0800

    v1.6.1

--- lib/submenu/wcmd-general.php
+++ lib/submenu/wcmd-general.php
@@ -53,7 +53,7 @@ if ( ! class_exists( 'WpssoWcmdSubmenuWcmdGeneral' ) && class_exists( 'WpssoAdmi
 
 			$tab_key = 'general';
 
-			$filter_name = SucomUtil::sanitize_hookname( $this->p->lca . '_' . $metabox_id . '_' . $tab_key . '_rows' );
+			$filter_name = SucomUtil::sanitize_hookname( 'wpsso_' . $metabox_id . '_' . $tab_key . '_rows' );
 
 			$table_rows = apply_filters( $filter_name, $this->get_table_rows( $metabox_id, $tab_key ), $this->form );
 
