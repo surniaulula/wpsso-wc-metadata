8c71ec1314b05e5b882918a7fcc53ff2e2bceaa0
JS Morisset <jsm@surniaulula.com>
2020-12-16 11:34:38 -0800

    v1.8.0

--- wpsso-wc-metadata.php
+++ wpsso-wc-metadata.php
@@ -11,7 +11,7 @@
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: GTIN, GTIN-8, GTIN-12 (UPC), GTIN-13 (EAN), GTIN-14, ISBN, MPN, depth, and volume for WooCommerce products and variations.
- * Requires PHP: 5.6
+ * Requires PHP: 7.0
  * Requires At Least: 4.5
  * Tested Up To: 5.6
  * WC Tested Up To: 4.8.0
