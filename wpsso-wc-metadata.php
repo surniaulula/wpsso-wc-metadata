221e3c633a9a1fa8331718978473469d970d1501
JS Morisset <jsm@surniaulula.com>
2021-08-26 11:54:42 -0700

    v1.9.0-b.1

--- wpsso-wc-metadata.php
+++ wpsso-wc-metadata.php
@@ -15,7 +15,7 @@
  * Requires At Least: 5.0
  * Tested Up To: 5.8
  * WC Tested Up To: 5.6.0
- * Version: 1.9.0-dev.2
+ * Version: 1.9.0-b.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
