00824a38d05176bf19c4542c4526a8fd550fed26
JS Morisset <jsm@surniaulula.com>
2022-01-16 11:05:18 -0800

    v1.13.0-dev.6

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoWcmdConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssowcmd' => array(			// Plugin acronym.
-					'version'     => '1.13.0-dev.4',	// Plugin version.
+					'version'     => '1.13.0-dev.6',	// Plugin version.
 					'opt_version' => '10',		// Increment when changing default option values.
 					'short'       => 'WPSSO WCMD',	// Short plugin name.
 					'name'        => 'WPSSO Product Metadata for WooCommerce SEO',
@@ -44,7 +44,7 @@ if ( ! class_exists( 'WpssoWcmdConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '9.14.0-dev.4',
+							'min_version'   => '9.14.0-dev.6',
 						),
 					),
 
