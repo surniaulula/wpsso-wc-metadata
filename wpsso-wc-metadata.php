0fe2f911d95251349860ceb8d8612680bc810a1b
JS Morisset <jsm@surniaulula.com>
2022-03-30 16:54:01 -0700

    v1.13.1

--- wpsso-wc-metadata.php
+++ wpsso-wc-metadata.php
@@ -13,7 +13,7 @@
  * Description: GTIN, GTIN-8, GTIN-12 (UPC), GTIN-13 (EAN), GTIN-14, ISBN, MPN, depth, and volume for WooCommerce products and variations.
  * Requires PHP: 7.2
  * Requires At Least: 5.2
- * Tested Up To: 5.9.2
+ * Tested Up To: 5.9.3
  * WC Tested Up To: 6.3.1
  * Version: 1.13.1
  *
