123e5d4d1deca0e3fa097c94e11cddf6d33bf96b
JS Morisset <jsm@surniaulula.com>
2023-02-04 08:13:05 -0800

    v3.1.1

--- wpsso-wc-metadata.php
+++ wpsso-wc-metadata.php
@@ -70,6 +70,9 @@ if ( ! class_exists( 'WpssoWcmd' ) ) {
 			load_plugin_textdomain( 'wpsso-wc-metadata', false, 'wpsso-wc-metadata/languages/' );
 		}
 
+		/*
+		 * Called by Wpsso->set_objects which runs at init priority 10.
+		 */
 		public function init_objects() {
 
 			$this->p =& Wpsso::get_instance();
