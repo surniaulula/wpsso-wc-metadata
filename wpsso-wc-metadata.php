0073ce3f81f2e0381f31c9308fd3ea5038e3d9f8
JS Morisset <jsm@surniaulula.com>
2023-04-26 06:43:33 -0700

    v3.1.1

--- wpsso-wc-metadata.php
+++ wpsso-wc-metadata.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: MPN, ISBN, GTIN, GTIN-8, UPC, EAN, GTIN-14, net dimensions, and fluid volume for WooCommerce products and variations.
  * Requires Plugins: wpsso, woocommerce
- * Requires PHP: 7.2
+ * Requires PHP: 7.2.5
  * Requires At Least: 5.5
  * Tested Up To: 6.2.0
  * WC Tested Up To: 7.6.0
