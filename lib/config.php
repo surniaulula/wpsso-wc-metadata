d03db7d92d8e5045ee8af3fc57838f41fcb159a1
JS Morisset <jsm@surniaulula.com>
2023-07-26 09:33:32 -0700

    v3.2.0-rc.1

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoWcmdConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssowcmd' => array(			// Plugin acronym.
-					'version'     => '3.2.0-b.1',	// Plugin version.
+					'version'     => '3.2.0-rc.1',	// Plugin version.
 					'opt_version' => '22',		// Increment when changing default option values.
 					'short'       => 'WPSSO WCMD',	// Short plugin name.
 					'name'        => 'WPSSO Product Metadata for WooCommerce SEO',
