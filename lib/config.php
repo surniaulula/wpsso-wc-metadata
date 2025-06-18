b4487cf4b0b1b610ca9cd04c04cc84b8bcb9c714
jsmoriss <jsm@surniaulula.com>
2025-06-18 16:48:08 +0000

    v5.0.0-rc.1

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoWcmdConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssowcmd' => array(			// Plugin acronym.
-					'version'     => '5.0.0-b.1',	// Plugin version.
+					'version'     => '5.0.0-rc.1',	// Plugin version.
 					'opt_version' => '22',		// Increment when changing default option values.
 					'short'       => 'WPSSO WCMD',	// Short plugin name.
 					'name'        => 'WPSSO Schema Product Metadata for WooCommerce',
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoWcmdConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '21.0.0-b.1',
+							'min_version'   => '21.0.0-rc.1',
 						),
 						'woocommerce' => array(
 							'name'          => 'WooCommerce',
